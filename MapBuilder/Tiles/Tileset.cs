33c4dd4cbed7a63a2f7357fb5d426c2bc4ba26d1
EdwinMindcraft <edwin.mindcraft@gmail.com>
2018-05-01 13:42:41 +0200

    Added Json

--- MapBuilder/Tiles/Tileset.cs
+++ MapBuilder/Tiles/Tileset.cs
@@ -9,23 +9,36 @@ namespace MapBuilder.Tiles {
 
 		public int TileSize { get; }
 		public List<Image> Tiles { get; }
+		public List<TileData> TilesData { get; }
 		public event EventHandler TileUpdated = new EventHandler((sender, args)=> { });
+
+		private int imageId = 0;
 
 		public Tileset(int tileSize) {
 			this.TileSize = tileSize;
 			this.Tiles = new List<Image>();
+			this.TilesData = new List<TileData>();
 		}
 
 		public void AddTileMap(Image image) {
-			Bitmap bitmap = new Bitmap(image); 
+			Bitmap bitmap = new Bitmap(image);
+			int currentImageID = this.imageId++;
 			for (int i = 0; i < image.Height - TileSize; i += TileSize) {
 				for (int j = 0; j < image.Width - TileSize; j += TileSize) {
 					Rectangle target = new Rectangle(j, i, TileSize, TileSize);
 					Tiles.Add(bitmap.Clone(target, System.Drawing.Imaging.PixelFormat.DontCare));
+					TilesData.Add(new TileData() { ImageID = imageId, X = i / TileSize, Y = j / TileSize, ID = Tiles.Count - 1});
 				}
 			}
 			if (TileUpdated != null)
 				TileUpdated.Invoke(this, new EventArgs());
 		}
+	}
+
+	public struct TileData {
+		public int ImageID { get; set; }
+		public int ID { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
 	}
 }
