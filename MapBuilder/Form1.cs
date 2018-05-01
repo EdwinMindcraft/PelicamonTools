8408ecf2fa54b622a3765166f6bc1c8d091e5f33
EdwinMindcraft <edwin.mindcraft@gmail.com>
2018-05-01 21:56:23 +0200

    Warning relocation

--- MapBuilder/Form1.cs
+++ MapBuilder/Form1.cs
@@ -7,31 +7,16 @@ using MapBuilder.Controls;
 namespace MapBuilder {
 
 	public partial class Form1 : Form {
-        const int TILESETS_NUMBER = 2;
 
 
         public Form1() {
 			InitializeComponent();
-			/*
-            for (int i = 0; i < TILESETS_NUMBER; i++)
-            {
-                this.tilesetPalette1.AvailableTilesets.Add(new Tileset(32));
-                this.tilesetPalette1.AvailableTilesets[i].Index = i;
-                this.tilesetPalette1.AvailableTilesets[i].TileUpdated += (sender, e) => { this.tilemapDesigner1.Tileset = this.tilesetPalette1.Tileset; };
-            }
 			//SANITY WARNING
 			//The following code might hurt your soul/eyes/brain (delete as appropriate) due to its ugliness
 			//Creators of this code is not responsible for any kind of physical/emotional/mental/other damage caused by it
 			//Therefore it is recommended to continue with caution
 			//WARNING END
-			this.tilesetPalette1.AvailableTilesets[0].AddTileMap(Properties.Resources.Outside);
-            this.tilesetPalette1.AvailableTilesets[0].AddTileMap(Properties.Resources.PlainColors, false);
-            this.tilesetPalette1.AvailableTilesets[0].Name = "Outside";
-            this.tilesetPalette1.AvailableTilesets[1].AddTileMap(Properties.Resources.PlainColors);
-            this.tilesetPalette1.AvailableTilesets[1].Name = "Plain Colors";*/
-            this.tilesetPalette1.FinishInitialisation();
-            //You are now exiting the 'Crappy Code Zone'
-            //If you're reading this message, congratulations for making it out alive.
+			this.tilesetPalette1.FinishInitialisation();
 			this.tilesetPalette1.OnTileSelect += (i, tileset) => { this.tilemapDesigner1.Selected = i; };
 			this.tilesetPalette1.OnTilesetChange += (ts) => {
 				this.tilemapDesigner1.Tileset = ts;
@@ -47,8 +32,11 @@ namespace MapBuilder {
 			this.tilemapDesigner1.UpdateLayerList();
 			this.tilemapDesigner1.Redraw();
 			this.tilesetPalette1.Redraw();
+			//You are now exiting the 'Crappy Code Zone'
+			//If you're reading this message, congratulations for making it out alive.
+			//Lol Just kidding, this whole program is a mess.
 		}
-		
+
 		private void binaryFileToolStripMenuItem_Click(object sender, System.EventArgs e) {
 			SaveFileDialog dialog = new SaveFileDialog();
 			dialog.Filter = "Tile Map Binaries|*.tmb";
