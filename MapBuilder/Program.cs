5c0204f77d4acf5251828456c0b5864f649c6174
Gawein Le Goff <gawein.le@live.fr>
2018-05-02 19:31:55 +0200

    Added update checker

--- MapBuilder/Program.cs
+++ MapBuilder/Program.cs
@@ -1,6 +1,8 @@
 ﻿using MapBuilder.Tiles;
 using System;
 using System.Windows.Forms;
+using MapBuilder.SubWindows;
+using MapBuilder.Utils;
 
 namespace MapBuilder {
 	static class Program {
@@ -9,16 +11,25 @@ namespace MapBuilder {
 		/// </summary>
 		public static Form1 FormInstance;
 		public static MasterTileset MasterTileset { get; set; } = new MasterTileset(32);
-		[STAThread]
-		static void Main() {
-			MasterTileset.AddChild("Outside", Properties.Resources.Outside);
-			MasterTileset.AddChild("Plain Colors", Properties.Resources.PlainColors);
-			MasterTileset.AddChild("Manual Convert", Properties.Resources.TilesConverted);
-			MasterTileset.AddChild("Size Test", 48, Properties.Resources.Tiles48);
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			FormInstance = new Form1();
-			Application.Run(FormInstance);
-		}
-	}
+        public static string Version = "0.0.1";
+        [STAThread]
+        static void Main()
+        {
+            #region Update Check
+            GitUtils.Init();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            UpdateChecker uCheckInstance = new UpdateChecker();
+            Application.Run(uCheckInstance);
+            #endregion;
+            #region Main Program Start
+            MasterTileset.AddChild("Outside", Properties.Resources.Outside);
+            MasterTileset.AddChild("Plain Colors", Properties.Resources.PlainColors);
+            MasterTileset.AddChild("Manual Convert", Properties.Resources.TilesConverted);
+            MasterTileset.AddChild("Size Test", 48, Properties.Resources.Tiles48);
+            FormInstance = new Form1();
+            Application.Run(FormInstance);
+            #endregion;
+        }
+    }
 }
---
+++ MapBuilder/SubWindows/UpdateChecker.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using MapBuilder.Utils;
+using Octokit;
+
+namespace MapBuilder.SubWindows
+{
+    public partial class UpdateChecker : Form
+    {
+        public UpdateChecker()
+        {
+            InitializeComponent();
+            //progressBar1.Style = ProgressBarStyle.Marquee;
+            //progressBar1.MarqueeAnimationSpeed = 30;
+            
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void UpdateChecker_Shown(object sender, EventArgs e)
+        {
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 300;
+            Thread th = new Thread(FetchUpdate);
+            th.Start();
+            th.Join();
+            progressBar1.MarqueeAnimationSpeed = 0;
+            this.Close();
+        }
+
+        private void FetchUpdate()
+        {
+            try
+            {
+                Release latestRelease = GitUtils.GetLatestVersion();
+                int[] latestVersion = GitUtils.DecomposeVersion(latestRelease);
+                int[] currentVersion = GitUtils.DecomposeVersion(MapBuilder.Program.Version);
+
+                if (GitUtils.CompareVersion(latestVersion, currentVersion))
+                {
+                    DialogResult choice = MessageBox.Show("A new version is available ! (Current : " + MapBuilder.Program.Version + " | Latest : " + latestRelease.TagName + ") \n Would you like to go to the downloads page ?", "Updater", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (choice == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("https://github.com/EdwinMindcraft/PelicamonTools/releases");
+                    }
+
+                }
+                else
+                {
+                    Console.Out.WriteLine("Current version matches latest release");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Seems like an error occured, please verify your connection and try again. \n If the problem still occurs, please contact EdwinMindcraft or Edern via Discord with the following error report attached. \n \n" + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
