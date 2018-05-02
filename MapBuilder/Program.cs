4f93f85a9cde2717c05343a16f37f17e208e5a46
EdwinMindcraft <edwin.mindcraft@gmail.com>
2018-05-02 16:24:32 +0200

    Multiple tilesets images now possible.

--- MapBuilder/Program.cs
+++ MapBuilder/Program.cs
@@ -2,25 +2,23 @@
 using System;
 using System.Windows.Forms;
 
-namespace MapBuilder
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        public static Form1 FormInstance;
+namespace MapBuilder {
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		public static Form1 FormInstance;
 		public static MasterTileset MasterTileset { get; set; } = new MasterTileset(32);
 		[STAThread]
-        static void Main() {
-			MasterTileset.AddChild(Properties.Resources.Outside, "Outside");
-			MasterTileset.AddChild(Properties.Resources.PlainColors, "Plain Colors");
-            MasterTileset.AddChild(Properties.Resources.TilesConverted, "Manual Convert");
-            MasterTileset.AddChild(Properties.Resources.Tiles48, "Size Test", 48);
+		static void Main() {
+			MasterTileset.AddChild("Outside", Properties.Resources.Outside);
+			MasterTileset.AddChild("Plain Colors", Properties.Resources.PlainColors);
+			MasterTileset.AddChild("Manual Convert", Properties.Resources.TilesConverted);
+			MasterTileset.AddChild("Size Test", 48, Properties.Resources.Tiles48);
 			Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            FormInstance = new Form1();
-            Application.Run(FormInstance);
-        }
-    }
+			Application.SetCompatibleTextRenderingDefault(false);
+			FormInstance = new Form1();
+			Application.Run(FormInstance);
+		}
+	}
 }
--- MapBuilder/Tiles/MasterTileset.cs
+++ MapBuilder/Tiles/MasterTileset.cs
@@ -12,28 +12,34 @@ namespace MapBuilder.Tiles {
 			this.Childs = new List<Tileset>();
 		}
 
-		public void AddChild(Image image, String name)
-        {
-			this.AddTileMap(image);
+		public void AddChild(String name, params Image[] images) {
 			Tileset child = new Tileset(this.TileSize);
 			int s = 0;
 			Childs.ForEach(ts => s += ts.Tiles.Count);
 			child.StartIndex = s;
 			child.Name = name;
-			child.AddTileMap(image);
+			images.ToList().ForEach(img => {
+				this.AddTileMap(img);
+				child.AddTileMap(img);
+			});
+			//this.AddTileMap(image);
+			//child.AddTileMap(image);
 			Childs.Add(child);
 		}
 
-        public void AddChild(Image image, String name, int size)
-        {
-            this.AddTileMap(image);
+        public void AddChild(String name, int size, params Image[] images) {
             Tileset child = new Tileset(size);
             int s = 0;
             Childs.ForEach(ts => s += ts.Tiles.Count);
             child.StartIndex = s;
             child.Name = name;
-            child.AddTileMap(image);
-            Childs.Add(child);
+			images.ToList().ForEach(img => {
+				this.AddTileMap(img);
+				child.AddTileMap(img);
+			});
+			//child.AddTileMap(image);
+			//this.AddTileMap(image);
+			Childs.Add(child);
         }
     }
 }
